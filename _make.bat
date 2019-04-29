6bad194ec66bc81267d9790bbc676bc0d55a3bb3
Anton Velichko <asve@ae-nest.com>
2019-04-29 15:19:04 +0300

    Сбори и подготовка материала.

--- _make.bat
+++ _make.bat
@@ -1,13 +1,4 @@
 set prog=main
-rem #%frompage=02
-rem #%topage=61
 
 for %%i in ( src\%prog%.asm ) do sjasmplus --labels %%i
 
-rem if EXIST %prog%.lab (
- rem # sed -i "s/${frompage}:/${topage}:/g" %prog%.lab
-rem sed -i "s/main\./m\./g" %prog%.lab
-rem sed -i "s/zifi\./z\./g" %prog%.lab
-rem sed -i "s/string\./s\./g" %prog%.lab
-rem sed -i "s/uart_ts_zifi\./utz\./g" %prog%.lab
-rem )
--- src/main.asm
+++ src/main.asm
@@ -24,8 +24,8 @@
 
 	define	OS_WINDOWS=1
 
- 	define buildSexyBoot				; Сборка загрузчика системы boot.$c
-; 	define buildKernel				; Сборка всей системы
+; 	define buildSexyBoot				; Сборка загрузчика системы boot.$c
+ 	define buildKernel				; Сборка всей системы
 ; 	define buildRes					; Сборка файлов ресурсов (Pal, Cur, Fnt, keymap)
 ; 	define buildTest				; Сборка тестового приложения test
 ; 	define buildEcho				; Сборка команды echo
